b8bffd137fbce19d55be42b521b27d558542ea83
GrapheneOS <contact@grapheneos.org>
2023-06-06 14:44:05 +0300

    gmscompat: add compatibility layer for unprivileged GMS
    Originally authored by Danny Lin <danny@kdrag0n.dev> for inclusion in
    GrapheneOS. It has since been substantially extended and partially
    rewritten by Dmitry Muhomor <muhomor.dmitry@gmail.com> (primarily) and
    Daniel Micay <danielmicay@gmail.com>.
    
    Commit history:
    (better viewed with git log --grep gmscompat $TAG_NAME)
    
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025081400
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025060200
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025021100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024112700
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024083100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024053100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024030300
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023112900
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023080800
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ2A.230505.002.2023052900
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ1A.230205.002.2023020600
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.006.2022073000
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.002.2022053100
    July 2021 - October 2021:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/RQ3A.211001.001.2021100606
    
    Change-Id: I2f4173fafa2b9a75e7db25e74a458abbec474f3a

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -41,6 +41,7 @@ import android.annotation.SystemApi;
 import android.annotation.SystemService;
 import android.annotation.TestApi;
 import android.annotation.UserIdInt;
+import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.compat.annotation.UnsupportedAppUsage;
@@ -94,6 +95,8 @@ import android.view.WindowInsetsController.Appearance;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.app.LocalePicker;
 import com.android.internal.app.procstats.ProcessStats;
+import com.android.internal.gmscompat.sysservice.GmcUserManager;
+import com.android.internal.gmscompat.GmsHooks;
 import com.android.internal.os.RoSystemProperties;
 import com.android.internal.os.TransferPipe;
 import com.android.internal.util.FastPrintWriter;
@@ -4494,7 +4497,11 @@ public class ActivityManager {
     public List<RunningAppProcessInfo> getRunningAppProcesses() {
         return mRunningProcessesCache.get(() -> {
             try {
-                return getService().getRunningAppProcesses();
+                List<RunningAppProcessInfo> res = getService().getRunningAppProcesses();
+                if (GmsCompat.isEnabled()) {
+                    res = GmsHooks.addRecentlyBoundPids(mContext, res);
+                }
+                return res;
             } catch (RemoteException e) {
                 throw e.rethrowFromSystemServer();
             }
@@ -5522,6 +5529,10 @@ public class ActivityManager {
     })
     @android.ravenwood.annotation.RavenwoodReplace
     public static int getCurrentUser() {
+        if (GmsCompat.isEnabled()) {
+            return GmcUserManager.amGetCurrentUser();
+        }
+
         return mGetCurrentUserIdCache.query(null);
     }
 
@@ -5846,6 +5857,10 @@ public class ActivityManager {
      */
     @UnsupportedAppUsage
     public boolean isUserRunning(int userId) {
+        if (GmsCompat.isEnabled()) {
+            return GmcUserManager.amIsUserRunning(userId);
+        }
+
         try {
             return getService().isUserRunning(userId, 0);
         } catch (RemoteException e) {
