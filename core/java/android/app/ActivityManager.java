0068ecc0a4a71133fc661d1423173e2ab4d0ba6d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-10-20 22:53:06 +0300

    gmscompat: add support for clearing caches of Java binder wrappers
    This is needed for overriding binder wrappers via GmsCompatLib.

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -6034,6 +6034,11 @@ public class ActivityManager {
         return IActivityManagerSingleton.get();
     }
 
+    /** @hide */
+    public static void clearCachedService() {
+        IActivityManagerSingleton.clear();
+    }
+
     private static IActivityTaskManager getTaskService() {
         return ActivityTaskManager.getService();
     }
